9e3ed5910848eac7af65a2fb756c29c986904aaf
llinaax <alinataranova7@gmail.com>
2023-03-21 11:45:05 +0200

    the final

--- Task1/Task1/Aplication.cs
+++ Task1/Task1/Aplication.cs
@@ -13,41 +13,84 @@ public class Aplication
 
     public void Asking()
     {
-        Console.WriteLine("Press the button that you need:");
-        var ask = Convert.ToInt32(Console.ReadLine());
-
-        switch (ask)
+        var i = true;
+        while (i)
         {
-            case 1:
-    
-                restaurantManagment.AddNewDish();
-                Console.WriteLine("____");
-                Console.WriteLine("MENU");
-                Console.WriteLine("____");
+            Console.WriteLine("Press the button that you need:");
+            var ask = Convert.ToInt32(Console.ReadLine());
 
-                restaurantManagment.PrintMenu();
+            switch (ask)
+            {
+                case 1:
 
-                break;
-            case 2:
+                    restaurantManagment.AddNewDish();
+                    Console.WriteLine("____");
+                    Console.WriteLine("MENU");
+                    Console.WriteLine("____");
 
-                restaurantManagment.AddNewIngredient();
-                Console.WriteLine("___________");
-                Console.WriteLine("Ingredients");
-                Console.WriteLine("___________");
+                    restaurantManagment.PrintMenu();
 
-                restaurantManagment.PrintIngredients();
+                    break;
+                case 2:
 
-                break;
-            case 3:
+                    restaurantManagment.AddNewIngredient();
+                    Console.WriteLine("___________");
+                    Console.WriteLine("Ingredients");
+                    Console.WriteLine("___________");
 
-                restaurantManagment.AddNewOrder();
-                Console.WriteLine("______");
-                Console.WriteLine("Orders");
-                Console.WriteLine("______");
-                    
-                restaurantManagment.PrintNewOrder();
+                    restaurantManagment.PrintIngredients();
 
-                break;
+                    break;
+                case 3:
+
+                    restaurantManagment.AddNewOrder();
+                    Console.WriteLine("______");
+                    Console.WriteLine("Orders");
+                    Console.WriteLine("______");
+
+                    restaurantManagment.PrintNewOrder();
+
+                    break;
+                case 4:
+                    Staff staff = new Staff();
+
+                    restaurantManagment.AddNewEmployee();
+                    Console.WriteLine("_____");
+                    Console.WriteLine("Employee");
+                    Console.WriteLine("_____");
+
+                    restaurantManagment.PrintNewEmployee();
+                    break;
+                case 5:
+
+                    restaurantManagment.AddNewTable();
+                    Console.WriteLine("_____");
+                    Console.WriteLine("Table");
+                    Console.WriteLine("_____");
+
+                    restaurantManagment.PrintNewTable();
+                    break;
+                case 6:
+
+                    restaurantManagment.AddNewVisitor();
+                    Console.WriteLine("_______");
+                    Console.WriteLine("Visitor");
+                    Console.WriteLine("_______");
+
+                    restaurantManagment.PrintNewVisitor();
+                    break;
+                case 7:
+                    Console.WriteLine("Staff");
+                    restaurantManagment.PrintNewEmployee();
+                    break;
+                case 8:
+                    Console.WriteLine("Visitors");
+                    restaurantManagment.PrintNewVisitor();
+                    break;
+                case 9:
+                    i = false;
+                    break;
+            }
         }
     }
 }
